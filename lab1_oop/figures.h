e108fabb1ff9818271d35c0f88f4bed37195d5e0
Артемий Толкишевский <artemiytolkisevskiy@Macintosh.local>
2024-03-24 02:56:36 +0300

    first commit

---
+++ lab1_oop/figures.h
@@ -0,0 +1,16 @@
+//
+// Created by Артемий Толкишевский on 23.03.2024.
+//
+
+#ifndef LAB1_OOP_FIGURES_H
+#define LAB1_OOP_FIGURES_H
+
+
+
+class figures {
+
+};
+
+
+
+#endif //LAB1_OOP_FIGURES_H
---
+++ lab1_oop/menu.h
@@ -0,0 +1,16 @@
+//
+// Created by Артемий Толкишевский on 23.03.2024.
+//
+
+#ifndef LAB1_OOP_MENU_H
+#define LAB1_OOP_MENU_H
+
+
+
+class menu {
+
+};
+
+
+
+#endif //LAB1_OOP_MENU_H
